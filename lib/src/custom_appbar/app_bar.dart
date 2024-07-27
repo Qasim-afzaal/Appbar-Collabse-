32e5af622671cede75ccdf7c07d310885db1f2d7
Qasim Afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-27 23:32:01 +0500

    Update app_bar.dart

--- lib/src/custom_appbar/app_bar.dart
+++ lib/src/custom_appbar/app_bar.dart
@@ -30,149 +30,153 @@ class CustomAppBar extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                dataText,
-                style: theme.textTheme.bodyLarge?.copyWith(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(
-                width: 35,
-                height: 35,
-                child: ClipOval(
-                  child: Image.network(
-                    userImageUrl,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return const Center(
-                        child: Icon(
-                          Icons.image,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
+              _buildDataText(theme),
+              _buildUserProfileImage(userImageUrl),
             ],
           ),
         ),
-        background: Stack(
+        background: _buildFlexibleSpaceBackground(theme, isDark, context),
+      ),
+    );
+  }
+
+  Text _buildDataText(ThemeData theme) {
+    return Text(
+      dataText,
+      style: theme.textTheme.bodyLarge?.copyWith(
+        color: Colors.black,
+        fontSize: 20,
+      ),
+    );
+  }
+
+  ClipOval _buildUserProfileImage(String userImageUrl) {
+    return ClipOval(
+      child: Image.network(
+        userImageUrl,
+        fit: BoxFit.cover,
+        errorBuilder: (context, error, stackTrace) {
+          return const Center(
+            child: Icon(
+              Icons.image,
+              color: Colors.white,
+              size: 30,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildFlexibleSpaceBackground(
+      ThemeData theme, bool isDark, BuildContext context) {
+    return Stack(
+      children: [
+        _buildTopBarImage(),
+        _buildGreetingRow(theme, context),
+        _buildWelcomeContainer(theme, isDark, context),
+      ],
+    );
+  }
+
+  SizedBox _buildTopBarImage() {
+    return SizedBox(
+      height: 220,
+      child: SizedBox(
+        height: 180,
+        child: Image.asset(
+          "assets/images/topbar2.png",
+          fit: BoxFit.fitWidth,
+          width: double.infinity,
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  Positioned _buildGreetingRow(ThemeData theme, BuildContext context) {
+    return Positioned(
+      left: 10,
+      top: 60,
+      right: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SizedBox(
-              height: 220,
-              child: Stack(
+            _buildGreetingText(theme),
+            _buildUserProfileImage(userImageUrl),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _buildGreetingText(ThemeData theme) {
+    return Row(
+      children: [
+        Text(
+          "Hello,",
+          style: theme.textTheme.bodyLarge?.copyWith(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        const SizedBox(width: 10),
+        Text(
+          userName,
+          style: theme.textTheme.titleLarge?.copyWith(
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Positioned _buildWelcomeContainer(
+      ThemeData theme, bool isDark, BuildContext context) {
+    return Positioned(
+      left: 20,
+      bottom: 10,
+      right: 20,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: GestureDetector(
+          onTap: () {
+            // Handle add property action
+          },
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 64,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color.fromARGB(255, 232, 229, 229),
+              ),
+              borderRadius: BorderRadius.circular(16),
+              color: isDark ? Colors.grey[50] : Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(2, 61, 236, 0.8),
+                  spreadRadius: -10,
+                  offset: Offset(0, -25),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 children: [
-                  SizedBox(
-                    height: 180,
-                    child: Image.asset(
-                      "assets/images/topbar2.png",
-                      fit: BoxFit.fitWidth,
-                      width: double.infinity,
-                      color: Colors.blue,
-                    ),
-                  ),
-                  Positioned(
-                    left: 10,
-                    top: 60,
-                    right: 10,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "Hello,",
-                                style: theme.textTheme.bodyLarge?.copyWith(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              Text(
-                                userName,
-                                style: theme.textTheme.titleLarge?.copyWith(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            width: 35,
-                            height: 35,
-                            child: ClipOval(
-                              child: Image.network(
-                                userImageUrl,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Center(
-                                    child: Icon(
-                                      Icons.image,
-                                      color: Colors.white,
-                                      size: 30,
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                  Text(
+                    "Welcome",
+                    style: theme.textTheme.titleLarge?.copyWith(
+                      color: Colors.black,
+                      fontSize: 14,
                     ),
                   ),
                 ],
               ),
             ),
-            Positioned(
-              left: 20,
-              bottom: 10,
-              right: 20,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: GestureDetector(
-                  onTap: () {
-                    // Handle add property action
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 64,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 232, 229, 229),
-                      ),
-                      borderRadius: BorderRadius.circular(16),
-                      color: isDark ? Colors.grey[50] : Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Color.fromRGBO(2, 61, 236, 0.8),
-                          spreadRadius: -10,
-                          offset: Offset(0, -25),
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            "Welcome",
-                            style: theme.textTheme.titleLarge?.copyWith(
-                              color: Colors.black,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
